ba87d7db6812d9c56ed2b94e8ca357cecc7d62e7
giorgigio1 <giorgigiorgdze93@gmail.com>
2023-10-18 22:35:43 +0400

    removed api folder

--- index.js
+++ index.js
@@ -27,8 +27,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.use("/", "<div> hello world </div>");
 app.use("/auth", authRoutes);
 app.use("/user", authMiddleware, userRoutes);
-
-export default app;
